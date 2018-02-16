ca24ee54b109dae88497f4bf93274f945afb1f0b
JavierBlesa <heavybm@gmail.com>
2018-02-16 16:45:48 +0100

    Versión que funciona, con las cosas del test

--- src/parserTSIP.c
+++ src/parserTSIP.c
@@ -2,101 +2,147 @@
 
 
 /**
-* \brief Read received raw data from COM port.
-*
-* This is a non-blocking read function. This means that only available received
-* data will be served. User may decide to call this function within a loop
-* until the desired amount of data is received.
-*
-* \param buffer Pointer to destination buffer where to copy received raw data.
-* \param count Maximum number of bytes to be read.
-* \return Number of read bytes. This value will always be <= count.
-*/
+ * \brief Read received raw data from COM port.
+ *
+ * This is a non-blocking read function. This means that only available received
+ * data will be served. User may decide to call this function within a loop
+ * until the desired amount of data is received.
+ *
+ * \param buffer Pointer to destination buffer where to copy received raw data.
+ * \param count Maximum number of bytes to be read.
+ * \return Number of read bytes. This value will always be <= count.
+ */
 int32_t uavnComRead(uint8_t * const buffer, const uint32_t count);
 /**
-* \brief Process valid data at application level.
-*
-* \param[in] buffer Const pointer where valid data is located. Valid data consists of ID1 + ID2
+ * \brief Process valid data at application level.
+ *
+ * \param[in] buffer Const pointer where valid data is located. Valid data consists of ID1 + ID2
 + Data
-* \param[in] numberOfBytes It is filled with the number bytes.
-*/
+ * \param[in] numberOfBytes It is filled with the number bytes.
+ */
 void ProcessValidData(const uint8_t * const buffer, const int32_t numberOfBytes);
 /**
-* \brief CRC32 calculation. No CRC check is performed.
-*
-* \param buffer Pointer to buffer containing data to process.
-* \param start Offset value of first data byte (within buffer) to be processed.
-* \param end Offset value of last data byte (within buffer) to be processed.
-* \return Calculated CRC value.
-*/
+ * \brief CRC32 calculation. No CRC check is performed.
+ *
+ * \param buffer Pointer to buffer containing data to process.
+ * \param start Offset value of first data byte (within buffer) to be processed.
+ * \param end Offset value of last data byte (within buffer) to be processed.
+ * \return Calculated CRC value.
+ */
 uint32_t crc32(const uint8_t * const buffer, const uint32_t start, const uint32_t end);
+/**
+ * \brief Parse received TSIP packets optimizing the used memory
+ * return PACKET_OK when a successful packet is received, or error in the other cases
+ */
+uint8_t parserTSIP ();
 
 int main () {
-
-	int32_t readed;
-	uint8_t tempBuffer;
-	uint8_t *commandBuffer;
-	uint8_t commandSize = 0;
-	int8_t crcByte = 3;
-	uint32_t crc32Calculated;
-
 	int status;
-	uint8_t i;
-	uint8_t j;
+
 
 	while(1) {
 
-		// Detect the first DLE
 
-		while ((readed = uavnComRead(&tempBuffer, 1)) == 0) {
-		}
+		parserTSIP ();
+	}
 
-		while (tempBuffer != DLE) {
-			readed = uavnComRead(&tempBuffer, 1);
-		}
+	return status;
 
-		// Detect the ETX
-
-		while ((readed = uavnComRead(&tempBuffer, 1)) == 0) {
-		}
-		commandBuffer = (uint8_t *) malloc(1);
-		//commandBuffer[commandSize++] = tempBuffer; //First DLE in not valid data
-
-		while(tempBuffer != ETX) {
-			commandBuffer[commandSize++] = tempBuffer;
-			while ((readed = uavnComRead(&tempBuffer, 1)) == 0) {
-			}
-			commandBuffer = (uint8_t *) realloc(commandBuffer, sizeof(commandBuffer)+1); //Last byte always empty
-		}
-
-		//DLE extraction
-
-		commandBuffer = (uint8_t *) realloc(commandBuffer, sizeof(commandBuffer)-2); //Last byte always empty and last DLE
-		commandSize -= 2;
+}
 
 
-		for (i = commandSize -1; i >= 0; i--) {
-			if(commandBuffer[i] == DLE) {
-				//Deletes the others DLE
-				for (j = i; j >= commandSize; j++) {
-					commandBuffer[j-1] = commandBuffer[j];
+// Detect the first DLE
+uint8_t parserTSIP () {
+
+	int32_t readed;
+	uint8_t checkByte;
+	uint8_t *commandBuffer;
+	uint8_t *tmpBuffer;
+	uint8_t commandSize = 0;
+	uint32_t crc32Calculated;
+
+	uint8_t status;
+	uint8_t i;
+	uint8_t endPacket = 0;
+
+
+	//TEST
+	uint8_t dataCounter = 0;
+
+	while ((readed = uavnComRead(&checkByte, dataCounter++)) == 0) {
+	}
+
+	while (checkByte != DLE) {
+		readed = uavnComRead(&checkByte, dataCounter++);
+	}
+
+	// Detect the ETX
+
+	while ((readed = uavnComRead(&checkByte, dataCounter++)) == 0) {
+	}
+	commandBuffer = (uint8_t *) malloc(1);
+
+	while (!endPacket) {
+
+
+		if (checkByte == DLE) {
+			while ((readed = uavnComRead(&checkByte, dataCounter++)) == 0) {}
+			if (checkByte == DLE) {
+				commandBuffer[commandSize++] = checkByte;
+				tmpBuffer = (uint8_t *) realloc(commandBuffer, sizeof(commandBuffer)+1);
+				if (!tmpBuffer) {
+					status = OUT_OF_MEMORY;
 				}
-				commandSize--;
+				commandBuffer = tmpBuffer;
 			}
-			if (crcByte >= 0) {
-				ucrc32Received.byte[crcByte--]; //little-endiand
+			else if (checkByte == ETX) {
+				tmpBuffer = (uint8_t *) realloc(commandBuffer, sizeof(commandBuffer)-1); //Remove last empty
+				if (!tmpBuffer) {
+					status = OUT_OF_MEMORY;
+				}
+				commandBuffer = tmpBuffer;
+				endPacket = 1;
 			}
+			else {
+				endPacket = 1;
+				status = PACKET_FORMAT_ERROR;
+			}
+		}
+		else {
+			commandBuffer[commandSize++] = checkByte;
+			tmpBuffer = (uint8_t *) realloc(commandBuffer, sizeof(commandBuffer)+1);
+			if (!tmpBuffer) {
+				status = OUT_OF_MEMORY;
+			}
+			commandBuffer = tmpBuffer;
 
 		}
 
+		while ((readed = uavnComRead(&checkByte, dataCounter++)) == 0) {}
+	}
+
+	if (status == PACKET_OK) {
+
+		// Extract CRC32
+		for (i=0; i < CRCSIZE; i++){
+			ucrc32Received.byte[i] = commandBuffer[commandSize - CRCSIZE + i];
+		}
 
 		// Check CRC32
-		crc32Calculated = crc32(commandBuffer, 0, commandSize);
+		crc32Calculated = crc32(commandBuffer, 0, commandSize - CRCSIZE);
 
-		if (crc32Calculated == ucrc32Received.crc32) {
-			ProcessValidData(commandBuffer, commandSize);
+		if (crc32Calculated != ucrc32Received.crc32) { //TODO-change to equal
+
+			////CRC extraction
+			commandBuffer = (uint8_t *) realloc(commandBuffer, sizeof(commandBuffer)-CRCSIZE); //Remove CRC
+			ProcessValidData(commandBuffer, commandSize - CRCSIZE);
+
+			status = PACKET_OK;
 		}
 
+		else {
+			status = PACKET_CRC_ERROR;
+		}
 	}
 
 	return status;
@@ -105,16 +151,28 @@ int main () {
 
 
 int32_t uavnComRead(uint8_t * const buffer, const uint32_t count){
-	int32_t readed = 0;
 
-	return readed;
+
+	memcpy (buffer, data+count, 1);
+
+
+	return 1;
 
 }
 
 uint32_t crc32(const uint8_t * const buffer, const uint32_t start, const uint32_t end){
 	int32_t readed = 0;
 
-		return readed;
+	return readed;
 
 }
+
+
+void ProcessValidData(const uint8_t * const buffer, const int32_t numberOfBytes) {
+
+
+}
+
+
+
 
--- src/parserTSIP.h
+++ src/parserTSIP.h
@@ -1,9 +1,14 @@
+#ifndef PARSERTSIP_H
+#define PARSERTSIP_H
+
+
 /**
  * Includes
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /**
@@ -13,6 +18,7 @@
 #define DLE 0x10
 #define ETX 0x03
 
+#define CRCSIZE 4
 /**
  * Variables
  */
@@ -22,4 +28,14 @@ union crc32Received {
 	uint8_t byte[4];
 } ucrc32Received;
 
+enum packetstauts {
+	PACKET_OK,
+	PACKET_FORMAT_ERROR,
+	PACKET_CRC_ERROR,
+	OUT_OF_MEMORY
+};
 
+
+uint8_t data[16] = { DLE, 0x01, 0x01, 0x04, 0x03, 0x10, 0x10, 0x04, 0x45, 0x46, 0x47, 0x48, 0x49, 0x50, DLE, ETX};
+
+#endif
